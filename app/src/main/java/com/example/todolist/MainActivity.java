31fbd9f6416b12b07c0346bfb14039858aa9df47
nihad6690 <nihalanidhruvesh@gmail.com>
2022-08-01 15:18:35 -0400

    adding acitivity to add task and also implementing the UI for the activity

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button addBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addBtn = (Button) findViewById(R.id.addBtn);
+        addBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick (View view){
+                addTaskActivity();
+            }
+        });
+    }
+
+    public void addTaskActivity(){
+        Intent intent = new Intent(this, addTask.class);
+        startActivity(intent);
+
     }
 }
