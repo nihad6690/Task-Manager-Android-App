099cbbe02fc9cde9fc99e78a87c6cbf451ff21c9
nihad6690 <nihalanidhruvesh@gmail.com>
2022-08-02 11:33:47 -0400

    when the user goes to the add task activity the information is now sent back to the main activity.

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -6,10 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Button addBtn;
-
+    public static final int TEXT_REQUEST  = 1;
+    public TextView setTitle;
+    private TextView setDescription;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,11 +24,28 @@ public class MainActivity extends AppCompatActivity {
                 addTaskActivity();
             }
         });
+        setTitle = findViewById(R.id.newTitle);
+        setDescription = findViewById(R.id.description);
     }
 
     public void addTaskActivity(){
         Intent intent = new Intent(this, addTask.class);
-        startActivity(intent);
+        startActivityForResult(intent, TEXT_REQUEST);
 
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == TEXT_REQUEST){
+                String title = data.getStringExtra(addTask.EXTRA_TITLE);
+                String description = data.getStringExtra(addTask.EXTRA_Description);
+                this.setTitle.setText(title);
+                this.setTitle.setVisibility(View.VISIBLE);
+                this.setDescription.setText(description);
+                this.setDescription.setVisibility(View.VISIBLE);
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/todolist/addTask.java
+++ app/src/main/java/com/example/todolist/addTask.java
@@ -2,13 +2,40 @@ package com.example.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class addTask extends AppCompatActivity {
+    private EditText editTitle;
+    private EditText editDescription;
+    private Button submitBtn;
+    public static final String EXTRA_TITLE = "com.example.todolist.EXTRA_TITLE";
+    public static final String EXTRA_Description = "com.example.todolist.EXTRA_Description";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
+        editTitle = findViewById(R.id.editTextTitle);
+        editDescription = findViewById(R.id.editTextDescription);
+        submitBtn = (Button) findViewById(R.id.submitbtn);
+        submitBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick (View view){
+
+                String title = editTitle.getText().toString();
+                String description = editDescription.getText().toString();
+                Intent output = new Intent();
+                output.putExtra(EXTRA_TITLE, title);
+                output.putExtra(EXTRA_Description, description);
+                setResult(RESULT_OK, output);
+                finish();
+            }
+        });
     }
 }
