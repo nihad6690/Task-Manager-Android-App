de7a57ce5e63912cb71fd9d07b86a30b9f1ef26e
nihad6690 <nihalanidhruvesh@gmail.com>
2022-08-05 14:25:34 -0400

    the sign in and sign up page work, so the user can sign in or sign up with their credentials

--- app/src/main/java/com/example/todolist/signInActivity.java
+++ app/src/main/java/com/example/todolist/signInActivity.java
@@ -1,14 +1,80 @@
 package com.example.todolist;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class signInActivity extends AppCompatActivity {
-
+    private Button signInBtn;
+    private EditText email;
+    private  EditText password;
+    private TextView createAccount;
+    FirebaseAuth firebaseAuth;
+    FirebaseDatabase firebaseDatabase;
+    ProgressDialog progressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        //getting instance of auth and database
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+
+        progressDialog = new ProgressDialog(signInActivity.this);
+        progressDialog.setTitle("Login");
+        progressDialog.setMessage("Logging in to your account");
+        signInBtn = findViewById(R.id.signInBtn);
+
+        email = findViewById(R.id.Email);
+        password = findViewById(R.id.password_edit);
+        createAccount = findViewById(R.id.accountCreated);
+
+        createAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(signInActivity.this, signUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressDialog.show();
+                firebaseAuth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                progressDialog.dismiss();
+                                if (task.isSuccessful()){
+                                    Intent intent = new Intent(signInActivity.this, MainActivity.class);
+                                    startActivity(intent);
+                                }
+                                else{
+                                    Toast.makeText(signInActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
+            }
+        });
+
+
+
     }
 }
--- app/src/main/java/com/example/todolist/signUpActivity.java
+++ app/src/main/java/com/example/todolist/signUpActivity.java
@@ -3,10 +3,13 @@ package com.example.todolist;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -20,10 +23,12 @@ public class signUpActivity extends AppCompatActivity {
     private EditText email;
     private  EditText password;
     private EditText username;
+    private TextView accountCreated;
 
 
     private FirebaseAuth mAuth;
     FirebaseDatabase database;
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,25 +38,39 @@ public class signUpActivity extends AppCompatActivity {
         //setup firebase
         mAuth = FirebaseAuth.getInstance();
         database = FirebaseDatabase.getInstance();
+        progressDialog = new ProgressDialog(signUpActivity.this);
+        progressDialog.setTitle("Creating the Account");
+        progressDialog.setMessage("Account is being created");
 
         //getting all buttons and editText
-        signUpBtn = findViewById(R.id.signUpBtn);
+        signUpBtn = findViewById(R.id.signInBtn);
         email = findViewById(R.id.email);
         password = findViewById(R.id.password_edit);
-        username = findViewById(R.id.username);
+        username = findViewById(R.id.Email);
+        accountCreated = findViewById(R.id.accountCreated);
+
+        accountCreated.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(signUpActivity.this, signInActivity.class);
+                startActivity(intent);
+            }
+        });
         signUpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                progressDialog.show();
                 mAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString()).
                         addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
+                                progressDialog.dismiss();
                                 if (task.isSuccessful()){
                                     Users user = new Users(username.getText().toString(), email.getText().toString(), password.getText().toString());
                                     String id = task.getResult().getUser().getUid();
                                     database.getReference("Users").child(id).setValue(user);
-
-
+                                    Intent intent = new Intent(signUpActivity.this, MainActivity.class);
+                                    startActivity(intent);
                                     Toast.makeText(signUpActivity.this, "User was created successfully", Toast.LENGTH_SHORT).show();
                                 }
                                 else{
